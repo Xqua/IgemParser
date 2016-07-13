0be6d38f485b18dbaae4017250a0712a11d024e0
Xqua In The Moon <xqua@free.fr>
2016-07-13 17:02:31 -0400

    added move

--- undelta.py
+++ undelta.py
@@ -19,7 +19,10 @@ main = [i for i in lfiles if 'tsv' in i][0]
 patches = [i for i in lfiles if 'patch' in i]
 
 for patch in patches:
+	print patch
     ts = patch.split('.')[0].split('_')[-1]
     n = 'page_view_db_%s.tsv' % ts
     cmd = 'xdelta patch %s %s %s' % (os.path.join(name, 'results', patch), os.path.join(name, 'results', main), os.path.join(name, 'results', n))
     os.system(cmd)
+
+os.system('mv %s/*.tsv pageViews2016' % os.path.join(name, 'results'))
