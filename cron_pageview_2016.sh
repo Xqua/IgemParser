844af168b91b71edcf3952f7bbe472a2948cc1f0
Xqua In The Moon <xqua@free.fr>
2016-07-13 16:37:20 -0400

    Modified cron parsing

--- cron_pageview_2016.sh
+++ cron_pageview_2016.sh
@@ -3,7 +3,11 @@
 if python wiki_crawler.py -v -y 2016 ; then
     echo "Command succeeded"
 else
-    echo "Command failed"
+    curl -X POST --data-urlencode 'payload={"text":"IGEM pageView parsing encountered an error !"}' https://commonground.xqua.org/hooks/SHxgYKqkQ6AkeTiMQ/YfjgCsRNrcFGrsMZFD3htn2YPSByukj5JPQb8FoPNgpDXPgp
 fi
+day_ts = `cat old_ts`  
 timestamp=$(date +%s)
-mv results/page_view_db.tsv results/page_view_db_$timestamp.tsv
+xdelta delta page_view_db_$day_ts.tsv page_view_db.tsv page_view_db_$timestamp.patch
+tar -uf results/page_view_db_$timestamp.tar page_view_db_$timestamp.patch
+rm page_view_db_$timestamp.patch
+
